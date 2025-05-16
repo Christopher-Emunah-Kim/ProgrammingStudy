05a114ee2706d24f572e468bf080a4b6f4421464
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-16 10:12:40 +0900

    update

--- ProgrammingStudy/ProgrammingStudy/996.DiceGame.cpp
+++ ProgrammingStudy/ProgrammingStudy/996.DiceGame.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
-
-int main()
-{
-	
-
-}
+//
+//int main()
+//{
+//	int dice1, dice2;
+//	int sum = 0;
+//
+//	srand((unsigned)time(NULL)); // 난수 초기화
+//	dice1 = rand() % 6 + 1; // 1~6 사이의 난수 생성
+//	dice2 = rand() % 6 + 1; // 1~6 사이의 난수 생성
+//
+//	sum = dice1 + dice2;
+//
+//	cout << "두 주사위 합 = " << sum << endl;
+//
+//}
