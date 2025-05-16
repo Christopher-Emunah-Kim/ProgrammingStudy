33d121f3512f0c9ac18f888cbe07cca37f574303
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-16 12:15:17 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/990. TestAverageArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int studentNum;
+	int ScoreArr[100];
+	int sum = 0;
+	int average;
+
+	cout << "학생 수를 입력하세요: ";
+	cin >> studentNum;
+
+	for (int i = 0; i < studentNum; i++)
+	{
+		cout << i + 1 << "번째 학생의 점수를 입력하세요: ";
+		cin >> ScoreArr[i];
+		sum += ScoreArr[i];
+	}
+	
+	average	 = int(sum  / studentNum);
+	cout << "학생들의 평균 점수는 " << average << "점입니다." << endl;
+
+
+	return 0;
+}
