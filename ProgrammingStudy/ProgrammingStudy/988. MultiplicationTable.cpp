9bf7539c61a420e9feda72a2f5b526273d0a3325
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-16 13:22:46 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/988. MultiplicationTable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+#define HEIGHT 3
+#define WIDTH 9
+
+//int main()
+//{
+//	//구구단 저장
+//	int table [HEIGHT][WIDTH];
+//
+//	//구구단 초기화
+//	for (int i = 0; i < HEIGHT; i++)
+//	{
+//		for (int j = 0; j < WIDTH; j++)
+//		{
+//			table[i][j] = (i + 2) * (j + 1);
+//		}
+//	}
+//	//구구단 출력
+//	for (int r = 0; r < HEIGHT; r++)
+//	{
+//		for (int c = 0; c < WIDTH; c++)
+//		{
+//			cout << table[r][c] << "\t";
+//		}
+//		cout << endl;
+//	}
+//
+//
+//	return 0;
+//}
--- ProgrammingStudy/ProgrammingStudy/989. FindBiggestNum.cpp
+++ ProgrammingStudy/ProgrammingStudy/989. FindBiggestNum.cpp
@@ -3,26 +3,25 @@
 using namespace std;
 
 
-int main()
-{
-	srand(time(NULL)); // 랜덤 시드 초기화
-
-	int arr[100];
-	for (int& i : arr)
-	{
-		int n = rand() % 100 + 1; // 1~100 사이의 랜덤 숫자 생성
-		arr[i] = n; // 배열에 랜덤 숫자 저장
-	}
-	// 배열의 최대값 찾기
-	int maxNum = arr[0];
-	for (int i : arr)
-	{
-		if (i > maxNum)
-		{
-			maxNum = i; // 최대값 갱신
-		}
-	}
-
-
-	return 0;
-}
+//int main()
+//{
+//	srand(time(NULL)); // 랜덤 시드 초기화
+//
+//	int arr[10];
+//	for (int& i : arr)
+//	{
+//		i = rand() % 100 + 1; // 1~100 사이의 랜덤 숫자 생성
+//	}
+//	// 배열의 최대값 찾기
+//	int maxNum = arr[0];
+//	for (int& i : arr)
+//	{
+//		if (i > maxNum)
+//		{
+//			maxNum = i; // 최대값 갱신
+//		}
+//	}
+//
+//
+//	return 0;
+//}
