a694ddae9814df2c4f7e5820ea3c88adc9e1c26b
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-18 23:03:29 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/978. Char Type2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <limits>
+//
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	char c1(65);
+//
+//	cout << sizeof(char) << endl;
+//	cout << (int) numeric_limits<char>::max() << endl;
+//	cout << (int) numeric_limits<char>::lowest() << endl;
+//
+//	cout << sizeof(unsigned char) << endl;
+//	cout << (int)numeric_limits<unsigned char>::max() << endl;
+//	cout << (int)numeric_limits<unsigned char>::lowest() << endl;
+//
+//	return 0;
+//}
