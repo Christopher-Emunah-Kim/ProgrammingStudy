ffdb08269ea1b03b228c128adb2790de0710f398
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-19 22:25:35 +0900

    update

--- ProgrammingStudy/ProgrammingStudy/Practice/954. cin_ignore_fail_clear.cpp
+++ ProgrammingStudy/ProgrammingStudy/Practice/954. cin_ignore_fail_clear.cpp
@@ -4,19 +4,38 @@ using namespace std;
 
 int getInt()
 {
-	cout << "Enter a integer number : ";
-	int x;
-	cin >> x;
+	while (true)
+	{
+		cout << "Enter an integer number : ";
+		int x;
+		cin >> x;
 
-	return x;
+		if (std::cin.fail()) // 잘못된 입력일 경우
+		{
+			std::cin.clear(); // 에러 플래그 제거
+			std::cin.ignore(32767, '\n'); // 입력 버퍼 비우기
+			cout << "Invalid Number" << endl;
+		}
+		else // 올바른 정수 입력 시
+		{
+			std::cin.ignore(32767, '\n'); // 나머지 입력 버퍼 비우기
+			return x; // 값을 반환하고 함수 종료
+		}
+	}
 }
 
 int getOperator()
 {
-	cout <<  "Enter an operator ( +, - ) : " ;
-	char op;
-	cin >> op;
-	return op;
+	while (true)
+	{
+		cout << "Enter an operator ( +, - ) : ";
+		char op;
+		cin >> op;
+		std::cin.ignore(32767, '\n');
+
+		if(op == '+' || op == '-') return op;
+		else cout << "Invalid operator" << endl;
+	}
 }
 
 void printResult(int x, char op, int y)
