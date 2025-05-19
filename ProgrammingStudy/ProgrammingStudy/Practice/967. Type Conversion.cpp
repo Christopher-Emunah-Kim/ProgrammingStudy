4c58c56d3587087047041c70f0e4d31860cd2a3d
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-19 18:15:16 +0900

    update

--- ProgrammingStudy/ProgrammingStudy/Practice/967. Type Conversion.cpp
+++ ProgrammingStudy/ProgrammingStudy/Practice/967. Type Conversion.cpp
@@ -1,34 +1,27 @@
-#include <iostream>
-
-
-using namespace std;
-
-int g_x; //external linakage
-
-static int g_y; //internal linkage
-
-//const int g_z; //X. 초기화 반드시해야함
-
-extern int g_z; //external linkage
-
-extern const int g_w; //O. 다른곳에서 반드시 한번은 초기화
-
-int g_p(1); //external linkage
-
-static int g_q(2); //internal linkage(정적 전역 변수)
-
-const int g_r(3); // 상수(파일 내에서만 사용가능한 상수)
-
-extern int g_s(1); //external linkage(다른곳에서 초기화x)
-
-extern const int g_t(1); //external linkage(다른곳에서도 쓸수있는 상수)
-
-
-
-int main()
-{
-	int i = 10;
-
-
-	return 0;
-}
+//#include <iostream>
+//#include <iomanip>
+//
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//	int a = 123.0; // int a = (int)123.0; // 암시적 형변환
+//	int b = (int)123.0;  // 명시적 형변환
+//	cout<< typeid(a).name() << endl;
+//
+//	float a = 1.0f;
+//	double d = a; //Numeric Promotion
+//
+//	int i = 30000;
+//	char c = i; //Numeric Conversion
+//	cout << static_cast<int>(c) << endl; // 44. 127도 안나옴.(절삭됨)
+//
+//	double d = 0.1234586938383;
+//	float f = d; //Numeric Conversion
+//	cout << std::setprecision(12) << f << endl; // 0.1234586938383
+//
+//
+//	return 0;
+//}
