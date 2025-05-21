87bcbb9d67f3b0cdcb57d89588c6c31c2d014a2e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-21 10:22:45 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/927. Default Parameters.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>  
+//
+//using namespace std;  
+//
+//
+//
+////기본값 초기화
+//void print(int x = 10, int y = 20, int z = 30)
+//{
+//
+//}
+//
+//
+////선언과 정의가 나뉘는 경우 기본값 초기화는 한곳에서만 진행해야함
+//void getValue(int a = 0, int b = 0); //in Header
+//
+//void getValue(int a, int b)
+//{
+//	//doSomething
+//}
+//
+//
+//int main()  
+//{  
+//	print();
+//	print(100);
+//	print(100, 200);
+//	print(100, 200, 300);
+//
+//
+//   return 0;  
+//}
--- ProgrammingStudy/ProgrammingStudy/Practice/928. Function Overloading.cpp
+++ ProgrammingStudy/ProgrammingStudy/Practice/928. Function Overloading.cpp
@@ -1,17 +1,26 @@
-#include <iostream>  
-
-using namespace std;  
-
-
-
-
-
-
-int main()  
-{  
-
-	cout << minValue(5,6) << endl;
-
-
-   return 0;  
-}
+//#include <iostream>  
+//
+//using namespace std;  
+//
+//int add(int x, int y)
+//{
+//	return x+y;
+//}
+//
+//double add(double x, double y)
+//{
+//	return x+y;
+//}
+//
+//void print(unsigned int value) {}
+//void print(float value) {}
+//
+//int main()  
+//{  
+//	add(5,6);
+//	add(3.14, 5.22);
+//
+//	//print('a'); //둘다 가능해서 컴파일러가 ambiguity 문제라 판단.
+//
+//   return 0;  
+//}
