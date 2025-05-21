b64f93db0484b35f97a0f0a8d23c1b61af0c4e0a
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-21 22:12:56 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/922. Command Line Arguments.cpp
@@ -0,0 +1,25 @@
+#include <iostream>  
+#include <string>
+
+using namespace std;  
+
+//argc: 실행 시 전달된 인자의 개수 (프로그램 이름 포함)
+//argv: 인자들을 담고 있는 문자열 배열
+int main(int argc, char *argv[])
+{
+	for (int count = 0; count < argc; ++count)
+	{
+		string argv_single = argv[count];
+
+		if (count == 1)
+		{
+			int input_number = stoi(argv_single);
+			cout << input_number + 1 << endl;
+		}
+		else
+			cout << argv_single << endl;
+	}
+
+	return 0;
+}
+
