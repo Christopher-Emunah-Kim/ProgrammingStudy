857268f71f6baf780a38b12a14a7d3bddc4d967a
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-21 09:30:31 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/929. Inline Function.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>  
+//
+//using namespace std;  
+//
+//inline int minValue(int x, int y)
+//{
+//	return  x>y?y:x;
+//}
+//
+//
+//
+//int main()  
+//{  
+//
+//	cout << minValue(5,6) << endl;
+//
+//
+//   return 0;  
+//}
--- ProgrammingStudy/ProgrammingStudy/Practice/930. Return_by_special thing.cpp
+++ ProgrammingStudy/ProgrammingStudy/Practice/930. Return_by_special thing.cpp
@@ -1,15 +1,40 @@
-#include <iostream>  
-#include <array>
-
-using namespace std;  
-
-
-
-int main()  
-{  
-
-
-
-
-   return 0;  
-}
+//#include <iostream>  
+//#include <array>
+//#include <tuple>
+//
+//using namespace std;  
+//
+////Return by Struct
+//struct S
+//{
+//	int a,b,c,d;
+//};
+//
+//S getStruct()
+//{
+//	S my_s{ 1,2,3,4 };
+//}
+//
+////Return by tuple
+//std::tuple<int, double> getTuple()
+//{
+//	int a = 10;
+//	double d = 3.14;
+//	return std::make_tuple(a, d);
+//}
+//
+//
+//int main()  
+//{  
+//	//Return by Struct
+//	//구현 오버헤드가 큼
+//	S my_s = getStruct();
+//	my_s.b;
+//
+//	//Return by tuple
+//	std::tuple<int, double> my_tp = getTuple();
+//	int a = std::get<0>(my_tp); //a
+//	double d = std::get<1>(my_tp); //b
+//
+//   return 0;  
+//}
