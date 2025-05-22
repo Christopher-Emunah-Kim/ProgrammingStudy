136b9ac88994a1fe7fc7e3ea1f5522c3e958ff32
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-22 15:11:30 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/915.Chaining Member Functions.cpp
@@ -0,0 +1,39 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//
+//
+//class Player {
+//private:
+//	int hp;
+//	int mp;
+//
+//public:
+//	Player& SetHP(int h)
+//	{
+//		hp = h; 
+//		return *this;
+//	}
+//
+//	Player& SetMP(int m)
+//	{
+//		mp = m;
+//		return *this;
+//	}
+//
+//	void Print() const {
+//		cout << "HP : " << hp << ", MP : " << mp << endl;
+//	}
+//};
+//
+//
+//
+//
+//int main() {
+//	
+//	Player p;
+//	p.SetHP(100).SetMP(50).Print(); //연쇄 호출
+//	
+//
+//	return 0;
+//}
