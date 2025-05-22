70cf0dfe01e2f960d9c13623cd4ce9a4ef5fce57
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-22 10:48:45 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/919. Object Slicing.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void Speak() const {
+		cout << "동물이 소리냅니다." << endl;
+	}
+};
+
+class Dog : public Animal {
+public:
+	void Speak() const override {
+		cout << "멍멍!" << endl;
+	}
+
+	void WagTail() const {
+		cout << "꼬리를 흔든다!" << endl;
+	}
+};
+
+void PrintAnimal(Animal animal) {  // ← 값으로 받음!
+	animal.Speak(); // 어떤 함수가 호출될까?
+}
+
+int main() {
+	Dog dog;
+	PrintAnimal(dog); // Dog 객체를 Animal 값으로 전달
+	return 0;
+}
+
+/*
+Dog 객체 메모리
+ ┌──────────────────────┐
+ │ Animal 부분 (vptr 등)│
+ │ Dog 고유 멤버들      │  ← 이 부분은 잘림!
+ └──────────────────────┘
+PrintAnimal(dog);에서 dog 객체를 값으로 전달하니,
+ Animal 클래스에 해당하는 부분만 복사되고,
+ Dog 클래스의 정보는 복사되지 않음.
+이로 인해 virtual 함수도 부모 기준으로 동작하게 되고,
+ → Dog::Speak()가 아닌 Animal::Speak()가 호출되는 것.
+
+원본객체의 vptr을 유지시키기 위해 반드시 참조나 포인터로 전달할것!
+*/
---
+++ ProgrammingStudy/ProgrammingStudy/Practice/920. Polymorphism 1.cpp
@@ -0,0 +1,65 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//// 기반 클래스 (부모)
+//class Animal {
+//public:
+//	virtual void Speak() const { // virtual: 다형성의 핵심!
+//		cout << "동물이 소리를 냅니다." << endl;
+//	}
+//};
+//
+//// 파생 클래스 1
+//class Dog : public Animal {
+//public:
+//	void Speak() const override {
+//		cout << "멍멍!" << endl;
+//	}
+//};
+//
+//// 파생 클래스 2
+//class Cat : public Animal {
+//public:
+//	void Speak() const override {
+//		cout << "야옹!" << endl;
+//	}
+//};
+//
+//// 다형성 사용
+//void MakeAnimalSpeak(const Animal& animal) {
+//	animal.Speak(); // 어떤 동물이든 이 함수 하나로 호출 가능
+//}
+//
+//int main() {
+//	Dog d; // 자식 인스턴스
+//	Cat c; // 자식 인스턴스
+//
+//	MakeAnimalSpeak(d); // "멍멍!" 출력
+//	MakeAnimalSpeak(c); // "야옹!" 출력
+//
+//
+//	/*
+//	dog 객체 메모리
+//	 ┌─────────────┐
+//	 │ vptr ------┐│   --> Dog의 vtable (함수 테이블)
+//	 └─────────────┘
+//
+//	Dog vtable
+//	 ┌──────────────────────────┐
+//	 │ 주소: &Dog::Speak()      │
+//	 └──────────────────────────┘
+//
+//	Animal vtable
+//	 ┌──────────────────────────┐
+//	 │ 주소: &Animal::Speak()   │
+//	 └──────────────────────────┘
+//
+//	//MakeAnimalSpeak(Animal& animal)에 dog 객체를 전달
+//	//animal.Speak() 호출
+//	//animal은 Animal& 타입이지만, 내부적으로 vptr은 Dog의 vtable을 가리킴
+//	//vptr → Dog의 vtable → Dog::Speak()로 연결되어 오버라이딩 된 함수가 호출됨
+//
+//	*/
+//
+//	return 0;
+//}
