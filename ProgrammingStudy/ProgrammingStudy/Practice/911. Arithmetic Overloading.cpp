106a792adee88b59a6874bfbf60842cb0d7ec3b5
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-05-23 21:59:44 +0900

    update

---
+++ ProgrammingStudy/ProgrammingStudy/Practice/911. Arithmetic Overloading.cpp
@@ -0,0 +1,45 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//
+//class Cents
+//{
+//private:
+//	int32_t m_cents;
+//
+//public:
+//	Cents(int32_t cents = 0) { m_cents = cents;  }
+//	Cents(int32_t cents) { m_cents = cents;  }
+//	int32_t getCents() const { return m_cents; }
+//
+//	int32_t& getCents() { return m_cents; }
+//
+//	/*friend Cents operator + (const Cents& c1, const Cents& c2)
+//	{
+//		return Cents(c1.getCents() + c2.getCents());
+//	}*/
+//
+//	Cents operator + (const Cents& other)
+//	{
+//		return Cents(this->m_cents + other.m_cents);
+//	}
+//};
+//
+//
+////Cents operator + (const Cents& c1, const Cents& c2)
+////{
+////	return Cents(c1.getCents() + c2.getCents());
+////}
+//
+//
+//int main()
+//{
+//	Cents cents1(6);
+//	Cents cents2(8);
+//
+//	cout << (cents1 + cents2 + Cents(6) + Cents(10)).getCents() << endl;
+//
+//
+//
+//	return 0;
+//}
--- ProgrammingStudy/ProgrammingStudy/Practice/912. Class Composition.cpp
+++ ProgrammingStudy/ProgrammingStudy/Practice/912. Class Composition.cpp
@@ -1,87 +1,87 @@
-﻿#include <iostream>
-using namespace std;
-
-//Class Composition
-//Has-A관계. 객체를 다른 객체의 멤버로 합류시켜 인터페이스만을 사용하도록 함.
-//결합도가 낮고 유연성이 높음(런타임 중 다른 객체로 교체 가능)
-//재사용성이 높음(다수의 기능객체를 조합 가능)
-
-//Class Inheritance(Is-A)관계의 한계(잘못된 설계 예시)
-//class Bird
+﻿//#include <iostream>
+//using namespace std;
+//
+////Class Composition
+////Has-A관계. 객체를 다른 객체의 멤버로 합류시켜 인터페이스만을 사용하도록 함.
+////결합도가 낮고 유연성이 높음(런타임 중 다른 객체로 교체 가능)
+////재사용성이 높음(다수의 기능객체를 조합 가능)
+//
+////Class Inheritance(Is-A)관계의 한계(잘못된 설계 예시)
+////class Bird
+////{
+////public:
+////	virtual void fly() { cout << "날 수 있다" << endl; }
+////
+////};
+////
+////
+////class Penguin : public Bird
+////{
+////public: 
+////	//fly() override ???
+////	//펭귄은 새지만(IS-A), 상속받은 기능인 fly는 부적절함.
+////};
+//
+//
+////Class Composition(HAS-A)관계 활용
+////날다(기능)
+//class FlyBehavior
 //{
 //public:
-//	virtual void fly() { cout << "날 수 있다" << endl; }
-//
+//	virtual void fly() = 0;
+//	virtual ~FlyBehavior() = default;
 //};
 //
+//class CanFly : public FlyBehavior
+//{
+//public :
+//	void fly() override
+//	{
+//		cout << "Can Fly" << endl;
+//	}
+//};
+//
+//class CannotFly : public FlyBehavior
+//{
+//public:
+//	void fly() override
+//	{
+//		cout << "Cannot Fly" << endl;
+//	}
+//};
+//
+//class Bird
+//{
+//protected :
+//	FlyBehavior* flyBehavior;
+//
+//public :
+//	Bird(FlyBehavior* fb) : flyBehavior{ fb } {}
+//	void doFly()
+//	{
+//		flyBehavior->fly();
+//	}
+//	virtual ~Bird()
+//	{
+//		delete flyBehavior;
+//	}
+//};
 //
 //class Penguin : public Bird
 //{
-//public: 
-//	//fly() override ???
-//	//펭귄은 새지만(IS-A), 상속받은 기능인 fly는 부적절함.
+//public:
+//	Penguin() : Bird(new CannotFly()) {}
 //};
-
-
-//Class Composition(HAS-A)관계 활용
-//날다(기능)
-class FlyBehavior
-{
-public:
-	virtual void fly() = 0;
-	virtual ~FlyBehavior() = default;
-};
-
-class CanFly : public FlyBehavior
-{
-public :
-	void fly() override
-	{
-		cout << "Can Fly" << endl;
-	}
-};
-
-class CannotFly : public FlyBehavior
-{
-public:
-	void fly() override
-	{
-		cout << "Cannot Fly" << endl;
-	}
-};
-
-class Bird
-{
-protected :
-	FlyBehavior* flyBehavior;
-
-public :
-	Bird(FlyBehavior* fb) : flyBehavior{ fb } {}
-	void doFly()
-	{
-		flyBehavior->fly();
-	}
-	virtual ~Bird()
-	{
-		delete flyBehavior;
-	}
-};
-
-class Penguin : public Bird
-{
-public:
-	Penguin() : Bird(new CannotFly()) {}
-};
-
-class Sparrow : public Bird
-{
-public:
-	Sparrow() : Bird(new CanFly()) {}
-};
-
-
-
-int main()
-{
-	return 0;
-}
+//
+//class Sparrow : public Bird
+//{
+//public:
+//	Sparrow() : Bird(new CanFly()) {}
+//};
+//
+//
+//
+//int main()
+//{
+//	return 0;
+//}
