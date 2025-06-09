783b889f18321919d90ffcbbd88efea4ea68f596
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-06-09 22:24:04 +0900

    <TEMP>

---
+++ ProgrammingStudy/ProgrammingStudy/Array.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+
+
+
+
+template<typename T, size_t size>
+class Array
+{
+public:
+
+
+
+private:
+	T data[size];
+
+public:
+	size_t Size() const
+	{
+		return size;
+	}
+
+	T& operator[](size_t index)
+	{
+		if (index >= size)
+		{
+			__debugbreak(); //디버그 모드에서만 작동
+		}
+
+		return data[index];
+	}
+
+	const T& operator [](size_t index) const 
+	{
+		if (index >= size)
+		{
+			__debugbreak(); //디버그 모드에서만 작동
+		}
+
+		return data[index];
+	}
+
+	T* Data()
+	{
+		return data;
+	}
+
+	const T* Data() const 
+	{
+		return data;
+	}
+};
+
+
+
+
+
+int main()
+{
+	Array<int, 5> data;
+
+	memset(data.Data(), 0, data.Size() * sizeof(int));
+
+	std::cin.get();
+
+	return 0;
+}
