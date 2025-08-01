03d996dc3d4f6325287c220b258665419df162b4
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-08-01 18:25:09 +0900

    sort

---
+++ ProgrammingStudy/ProgrammingStudy/DataStructure/bubblesort.cpp
@@ -0,0 +1,19 @@
+﻿void BublleSort(int* arr, int num)
+{
+	for (int i = 0; i < num - 1; i++)
+	{
+		bool swapped = false;
+		for (int j = 0; j < num - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				std::swap(arr[j], arr[j + 1]);
+				swapped = true;
+			}
+		}
+		if (swapped)
+		{
+			break;
+		}
+	}
+}
---
+++ ProgrammingStudy/ProgrammingStudy/DataStructure/selectionsort.cpp
@@ -0,0 +1,21 @@
+﻿template <typename T>
+void SelectionSort(T* arr, int num)
+{
+	for (int i = 0; i < num - 1; ++i)
+	{
+		int min_idx = i;
+
+		for (int j = i + 1; j < num; ++j)
+		{
+			if (arr[j] < arr[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+
+		if (min_idx != i)
+		{
+			std::swap(arr[i], arr[min_idx]);
+		}
+	}
+}
