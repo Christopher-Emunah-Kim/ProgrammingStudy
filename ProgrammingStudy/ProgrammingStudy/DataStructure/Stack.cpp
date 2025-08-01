743a2b61bded6cb96eddf18b7f62ed4e991c4a15
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-08-01 17:43:18 +0900

    stack

---
+++ ProgrammingStudy/ProgrammingStudy/DataStructure/Stack.cpp
@@ -0,0 +1,46 @@
+﻿
+int* stack;
+int size;
+int top = -1;
+
+void InitStack(int newSize)
+{
+	size = newSize;
+	stack = new int[newSize];
+}
+
+
+void DeleteStack()
+{
+	delete[] stack;
+	stack = nullptr;
+	size = 0;
+	top = -1;
+}
+
+
+bool Push(int data)
+{
+	if(top < size - 1)
+	{
+		top++;
+		stack[top] = data;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int Pop()
+{
+	if (top >= 0)
+	{
+		return stack[top--];
+	}
+	else
+	{
+		return -1;
+	}
+}
