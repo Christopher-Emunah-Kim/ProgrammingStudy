125224cdf42804ea04a78c89a0289b7b7a9cd6b6
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2025-08-01 18:08:57 +0900

    binarysearch

---
+++ ProgrammingStudy/ProgrammingStudy/DataStructure/BinarySearch.cpp
@@ -0,0 +1,25 @@
+﻿int BinarySearch(int* arr, int size, int key)
+{
+	int lower = 0;
+	int upper = size;
+
+	while (lower < upper)
+	{
+		int mid = lower + (upper - lower) / 2;
+
+		if (arr[mid] == key)
+		{
+			return mid;
+		}
+		else if (arr[mid] > key)
+		{
+			upper = mid;
+		}
+		else
+		{
+			lower = mid + 1;
+		}
+	}
+
+	return -1;
+}
